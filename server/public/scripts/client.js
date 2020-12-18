40ea0dc108e5ae7c07c386f16bfcb0146b840b17
Kevin Burk <percburk@gmail.com>
2020-12-18 16:32:59 -0600

    mathObject functioning correctly

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,19 +3,31 @@ $(onReady);
 function onReady() {
   console.log('JQ');
   $('.operator').on('click', assignOperator);
-  $('#enter').on('click', sendData);
+  $('#enter').on('click', sendMathObject);
   $('#clear').on('click', clearInputs);
 }
 
+let mathObject = {};
+
 function assignOperator() {
-  console.log('clicked an operator!');
-  console.log($(this).data('operator'));
+  delete mathObject.operator;
+  console.log('clicked', $(this).data('operator'));
+  let operator = $(this).data('operator');
+  mathObject.operator = operator;
 }
 
-function sendData() {
+function sendMathObject() {
   console.log('clicked enter!');
+  delete mathObject.numberOne;
+  delete mathObject.numberTwo;
+  mathObject.numberOne = Number($('#numberOne').val());
+  mathObject.numberTwo = Number($('#numberTwo').val());
+  console.log(mathObject);
+
+
 }
 
 function clearInputs() {
   console.log('clicked clear!');
+  $('.inputContainer input').val('');
 }
