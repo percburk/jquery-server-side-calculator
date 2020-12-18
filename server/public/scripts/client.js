393d9f50ee6f9d126520a18291df3139e50653e1
Kevin Burk <percburk@gmail.com>
2020-12-18 16:18:35 -0600

    click listeners working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,20 @@ $(onReady);
 
 function onReady() {
   console.log('JQ');
+  $('.operator').on('click', assignOperator);
+  $('#enter').on('click', sendData);
+  $('#clear').on('click', clearInputs);
+}
+
+function assignOperator() {
+  console.log('clicked an operator!');
+  console.log($(this).data('operator'));
+}
+
+function sendData() {
+  console.log('clicked enter!');
+}
+
+function clearInputs() {
+  console.log('clicked clear!');
 }
